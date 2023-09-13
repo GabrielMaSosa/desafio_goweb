d11a8f49eff7e1eb60b8fb959e0d2582483d1394
GabrielMaSosa <142988457+GabrielMaSosa@users.noreply.github.com>
2023-09-13 10:10:02 -0300

    fin

---
+++ cmd/router/router.go
@@ -0,0 +1,35 @@
+package router
+
+import (
+	"github.com/bootcamp-go/desafio-go-web/cmd/handler"
+	"github.com/bootcamp-go/desafio-go-web/internal/domain"
+	"github.com/bootcamp-go/desafio-go-web/internal/tickets"
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter(g *gin.Engine, list []domain.Ticket) Mhandler {
+	//aca instanciamos los repositorios
+	repo := tickets.NewRepository(list)
+
+	servi := tickets.NewService(repo)
+	mhdnler := handler.NewService(servi)
+
+	return Mhandler{
+		servhdnler: mhdnler,
+		servicio:   servi,
+		dta:        g,
+	}
+}
+
+type Mhandler struct {
+	servhdnler *handler.Service
+	servicio   tickets.Service
+	dta        *gin.Engine
+}
+
+func (h *Mhandler) MapRoutes() {
+	h.servhdnler.AverageDestination()
+	mygroup := h.dta.Group("/ticket")
+	mygroup.GET("/getByCountry/:dest", h.servhdnler.GetTicketsByCountry())
+	mygroup.GET("/getAverage/:dest", h.servhdnler.AverageDestination())
+}
